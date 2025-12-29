571e7c6e6cb2da1b73026ae1f93353656e224d29
sou963 <souravbosu844@gmail.com>
2025-12-29 22:06:12 +0600

    initial

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/api/news", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://newsapi.org/v2/everything?q=bangladesh&apiKey=${process.env.NEWS_API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: "News fetch failed" });
+  }
+});
+
+export default app;
